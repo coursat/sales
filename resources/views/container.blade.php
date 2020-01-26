2a39e10b243ec15e8c31568695f52d24685f544f
Mohammed <info@inoledge.net>
2020-01-26 23:31:40 +0200

    wow

---
+++ resources/views/container.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Dark Bootstrap Admin by Bootstrapious.com</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="robots" content="all,follow">
+    <!-- Bootstrap CSS-->
+    <link rel="stylesheet" href="{{url('custom')}}/vendor/bootstrap/css/bootstrap.min.css">
+    <!-- Font Awesome CSS-->
+    <link rel="stylesheet" href="{{url('custom')}}/vendor/font-awesome/css/font-awesome.min.css">
+    <!-- Custom Font Icons CSS-->
+    <link rel="stylesheet" href="{{url('custom')}}/css/font.css">
+    <!-- Google fonts - Muli-->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Muli:300,400,700">
+    <!-- theme stylesheet-->
+    <link rel="stylesheet" href="{{url('custom')}}/css/style.default.css" id="theme-stylesheet">
+    <!-- Custom stylesheet - for your changes-->
+    <link rel="stylesheet" href="{{url('custom')}}/css/custom.css">
+    <!-- Favicon-->
+    <link rel="shortcut icon" href="{{url('custom')}}/img/favicon.ico">
+    <!-- Tweaks for older IEs--><!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
+  </head>
+  <body>
+    @include('inc.header')
+
+
+    <div class="d-flex align-items-stretch">
+    @include('inc.sidebar')
+    @yield('content')
+    </div>
+
+
+    @include('inc.footer')
+
+  </body>
+</html>
---
+++ resources/views/inc/footer.blade.php
@@ -0,0 +1,8 @@
+<script src="{{url('custom')}}/vendor/jquery/jquery.min.js"></script>
+<script src="{{url('custom')}}/vendor/popper.js/umd/popper.min.js"> </script>
+<script src="{{url('custom')}}/vendor/bootstrap/js/bootstrap.min.js"></script>
+<script src="{{url('custom')}}/vendor/jquery.cookie/jquery.cookie.js"> </script>
+<script src="{{url('custom')}}/vendor/chart.js/Chart.min.js"></script>
+<script src="{{url('custom')}}/vendor/jquery-validation/jquery.validate.min.js"></script>
+<script src="{{url('custom')}}/js/charts-home.js"></script>
+<script src="{{url('custom')}}/js/front.js"></script>
